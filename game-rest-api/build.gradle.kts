c106006d696750bc5575adfbcd80a783e01c9336
Fireentity <croceclaudio57@gmail.com>
2023-04-27 17:47:50 +0200

    Chore: Aggiunti script gradle per game-rest-api e solver-rest-api

--- game-rest-api/build.gradle.kts
+++ game-rest-api/build.gradle.kts
@@ -1,4 +1,35 @@
 plugins {
     java
+    id("org.springframework.boot") version "3.0.2"
+    id("io.spring.dependency-management") version "1.1.0"
     id("klotski-web.java-conventions")
+}
+
+group = "org.klotski.web"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_19
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-devtools")
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("org.postgresql:postgresql")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
