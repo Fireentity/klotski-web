21fe0818f97d1032da9228d24d72dd175a84ce22
Fireentity <croceclaudio57@gmail.com>
2023-04-27 17:49:46 +0200

    Feat: Aggiunto componente card per vue

---
+++ web-client/src/components/Card.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
+        <div class="px-4 py-5 sm:px-6">
+            <slot name="header"/>
+        </div>
+        <div class="px-4 py-5 sm:p-6">
+            <slot name="main"/>
+        </div>
+    </div>
+</template>
