90d556e247465228474896716a127fe0aa3a3f87
Fireentity <croceclaudio57@gmail.com>
2023-05-06 12:53:01 +0200

    Refactor: Aggiunta path generica per  la scansione dei package

--- game-rest-api/src/it/klotski/web/game/GameRestApiApplication.java
+++ game-rest-api/src/it/klotski/web/game/GameRestApiApplication.java
@@ -5,12 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
-@SpringBootApplication(scanBasePackages = {"it.klotski.web.game.controllers",
-        "it.klotski.web.game.services",
-        "it.klotski.web.game.repositories",
-        "it.klotski.web.game.domain",
-        "it.klotski.web.game.security",
-        "it.klotski.web.game.handler"})
+@SpringBootApplication(scanBasePackages = {"it.klotski.web.game.*"})
 public class GameRestApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(GameRestApiApplication.class, args);
