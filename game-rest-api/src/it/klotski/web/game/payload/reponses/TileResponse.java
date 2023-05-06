39599af94b22d07ff106e385ae5585d54a76f221
Fireentity <croceclaudio57@gmail.com>
2023-05-06 12:49:48 +0200

    Refactor: Rimosso TileResponse

--- game-rest-api/src/it/klotski/web/game/payload/reponses/TileResponse.java
+++
@@ -1,9 +0,0 @@
-package it.klotski.web.game.payload.reponses;
-
-import it.klotski.web.game.config.Tile;
-
-public record TileResponse(long id, int x, int y, int width, int height) {
-    public static TileResponse from(Tile tile) {
-        return new TileResponse(tile.id(), tile.x(), tile.y(), tile.width(), tile.height());
-    }
-}
