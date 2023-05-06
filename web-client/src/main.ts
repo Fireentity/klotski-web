9b4639b34a25eac305281159a76f462006e493a9
Fireentity <croceclaudio57@gmail.com>
2023-05-06 14:41:39 +0200

    Feat: Aggiunta inizializzazione di vuex anche nel main

--- web-client/src/main.ts
+++ web-client/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from "./App.vue";
-import router from "@/routes/router.ts";
+import router from "@/routes/Router.ts";
+import store from "@/store/Store.ts"
 import './style.css'
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
