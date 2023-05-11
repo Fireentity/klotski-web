065d8247d79cf502088167c7efd5afa2e88b199a
Fireentity <croceclaudio57@gmail.com>
2023-05-11 22:31:45 +0200

    Refactor: Aggiunta la classe WinningTile per il tile di vittoria

--- game-rest-api/src/it/klotski/web/game/constants/ApplicationConstants.java
+++ game-rest-api/src/it/klotski/web/game/constants/ApplicationConstants.java
@@ -2,10 +2,16 @@ package it.klotski.web.game.constants;
 
 import it.klotski.web.game.domain.tile.ITile;
 import it.klotski.web.game.domain.tile.RectangularTile;
+import it.klotski.web.game.domain.tile.WinningTile;
 import it.klotski.web.game.utils.RuntimeTypeAdapterFactory;
 
+import java.text.SimpleDateFormat;
+
 public class ApplicationConstants {
+    public static final int DEFAULT_PAGE_SIZE = 20;
+    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy");
     public static final String START_CONFIGURATIONS_FILE_PATH = "classpath:config/startConfigurations.json";
     public static final RuntimeTypeAdapterFactory<ITile> ADAPTER_FACTORY = RuntimeTypeAdapterFactory.of(ITile.class, "type")
-            .registerSubtype(RectangularTile.class, "rectangular");
+            .registerSubtype(RectangularTile.class, "rectangular")
+            .registerSubtype(WinningTile.class, "winning");
 }
---
+++ game-rest-api/src/it/klotski/web/game/domain/tile/IWinningTile.java
@@ -0,0 +1,4 @@
+package it.klotski.web.game.domain.tile;
+
+public interface IWinningTile extends ITile, IRectangularTile {
+}
