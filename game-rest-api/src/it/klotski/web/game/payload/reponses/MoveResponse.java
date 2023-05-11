7b330e6958bb07f49989aadeae0742f39e6f582b
Fireentity <croceclaudio57@gmail.com>
2023-05-11 23:22:08 +0200

    Feat: Aggiunta MoveResponse

---
+++ game-rest-api/src/it/klotski/web/game/payload/reponses/MoveResponse.java
@@ -0,0 +1,14 @@
+package it.klotski.web.game.payload.reponses;
+
+import it.klotski.web.game.domain.tile.ITile;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.TreeSet;
+
+@Getter
+@RequiredArgsConstructor
+public class MoveResponse {
+    private final TreeSet<ITile> tiles;
+
+}
