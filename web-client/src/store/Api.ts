d220eec4165aedcbe039fa8792098ebec5a75742
Fireentity <croceclaudio57@gmail.com>
2023-05-12 19:40:54 +0200

    Refactor: Spostata inizializzazione di axios

--- web-client/src/store/Api.ts
+++
@@ -1,9 +0,0 @@
-import Axios from 'axios'
-
-const axios = Axios.create({
-    baseURL: 'http://localhost:8000/api',
-    responseType: 'json', // default
-})
-axios.defaults.xsrfCookieName = undefined
-axios.defaults.withCredentials = true
-export default axios
