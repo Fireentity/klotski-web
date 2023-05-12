8b740ff4a617694100a8a9e6e31cd5c23ecd48a4
Fireentity <croceclaudio57@gmail.com>
2023-05-12 19:36:51 +0200

    Feat: Aggiunta richiesta del token csrf all'avvio dell'applicazione

--- web-client/src/App.vue
+++ web-client/src/App.vue
@@ -2,6 +2,9 @@
 
 import {defineComponent} from "vue";
 import AppLayout from "@/components/layouts/AppLayout.vue";
+import store from "@/store/Store.ts";
+
+store.dispatch('csrf')
 
 export default defineComponent({
     components: {AppLayout}
