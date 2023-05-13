7b3f17c4628f202801e1facf7694dcc40866da57
Biblink <alby.bot.24@gmail.com>
2023-05-13 23:31:37 +0200

    Docs: Aggiunta la documentazione per la classe LoginRequest

--- game-rest-api/src/it/klotski/web/game/payload/requests/LoginRequest.java
+++ game-rest-api/src/it/klotski/web/game/payload/requests/LoginRequest.java
@@ -4,6 +4,9 @@ package it.klotski.web.game.payload.requests;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * Classe per la gestione delle richieste di login di un utente.
+ */
 @Getter
 @RequiredArgsConstructor
 public class LoginRequest {
