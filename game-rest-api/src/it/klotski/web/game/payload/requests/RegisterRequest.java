bae4eab3dadc90e2541532f05cf5055a6b216f62
Biblink <alby.bot.24@gmail.com>
2023-05-13 23:31:15 +0200

    Docs: Aggiunta la documentazione per la classe RegisterRequest

--- game-rest-api/src/it/klotski/web/game/payload/requests/RegisterRequest.java
+++ game-rest-api/src/it/klotski/web/game/payload/requests/RegisterRequest.java
@@ -3,6 +3,9 @@ package it.klotski.web.game.payload.requests;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * Classe per la gestione delle richieste di creazione di un nuovo utente (registrazione).
+ */
 @Getter
 @RequiredArgsConstructor
 public class RegisterRequest {
