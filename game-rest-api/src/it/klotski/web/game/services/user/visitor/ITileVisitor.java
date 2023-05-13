220e48f6b58f4441617c3c10da2034f029eb5bf0
Biblink <alby.bot.24@gmail.com>
2023-05-13 17:12:42 +0200

    Docs: Aggiunta la documentazione per l'interfaccia ITileVisitor'

--- game-rest-api/src/it/klotski/web/game/services/user/visitor/ITileVisitor.java
+++ game-rest-api/src/it/klotski/web/game/services/user/visitor/ITileVisitor.java
@@ -3,6 +3,10 @@ package it.klotski.web.game.services.user.visitor;
 import it.klotski.web.game.domain.tile.IRectangularTile;
 import it.klotski.web.game.domain.tile.IWinningTile;
 
+/**
+ * Interfaccia che utilizza il design pattern "Visitors" per distinguere i pezzi dal loro comportamento, in modo da
+ * associare a una determinata forma un determinato comportamento.
+ */
 public interface ITileVisitor {
     default void visitRectangularTile(IRectangularTile tileConfig) {
 
