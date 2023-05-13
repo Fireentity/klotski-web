598c360a68eba329ade493e742ad882e269938c3
Biblink <alby.bot.24@gmail.com>
2023-05-13 17:19:28 +0200

    Docs: Aggiunta la documentazione per la classe RectangularTileVisitor'

--- game-rest-api/src/it/klotski/web/game/services/user/visitor/RectangularTileVisitor.java
+++ game-rest-api/src/it/klotski/web/game/services/user/visitor/RectangularTileVisitor.java
@@ -2,14 +2,22 @@ package it.klotski.web.game.services.user.visitor;
 
 import it.klotski.web.game.domain.tile.IRectangularTile;
 import it.klotski.web.game.services.user.strategy.IRectangularTileStrategy;
-
+/**
+ * Visitor per la classe RectangularTile
+ */
 public class RectangularTileVisitor implements ITileVisitor {
     private final IRectangularTileStrategy insertionStrategy;
-
+    /**
+     * Costruttore di classe:
+     * @param insertionStrategy strategia generica dei tiles di tipo IRectangularTile
+     */
     public RectangularTileVisitor(IRectangularTileStrategy insertionStrategy) {
         this.insertionStrategy = insertionStrategy;
     }
-
+    /**
+     * Funzione per la visita degli oggetti di tipo IRectangularTile
+     * @param tileConfig oggetto che deve essere visitato
+     */
     @Override
     public void visitRectangularTile(IRectangularTile tileConfig) {
         insertionStrategy.apply(tileConfig);
