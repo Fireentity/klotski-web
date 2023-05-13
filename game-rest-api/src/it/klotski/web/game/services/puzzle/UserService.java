0a4582313403c3f0b4d4afd1672e165592f100fb
Biblink <alby.bot.24@gmail.com>
2023-05-13 16:14:48 +0200

    Docs: Aggiunta la documentazione per la classe UserService

--- game-rest-api/src/it/klotski/web/game/services/puzzle/UserService.java
+++ game-rest-api/src/it/klotski/web/game/services/puzzle/UserService.java
@@ -10,23 +10,44 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+
+/**
+ * Service per gestire la ricerca o l'immissione di utenti all'interno del database.
+ */
 @Service
 public class UserService implements UserDetailsService {
     private final IUserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    /**
+     * Costruttore di classe:
+     * @param userRepository è la tabella degli utenti.
+     * @param passwordEncoder è l'oggetto per crittografare tramite funzione di hash.
+     */
     public UserService(IUserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
+    /**
+     * Funzione per cercare un utente nel database in base all'email.
+     * @param email stringa contenente l'email da cercare all'interno del database.
+     * @return l'user, se esiste, associato alla email data in input.
+     * @throws UsernameNotFoundException nel caso di utente con tale email non presente.
+     */
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return userRepository.findByEmail(email)
-                .orElseThrow(() ->
+                .orElseThrow(() -> //funzione che ritorna se presente il valore cercato, sennò lancia una data eccezione.
                         new UsernameNotFoundException("User not found with username or email: " + email));
     }
 
+    /**
+     * Funzione per creare un nuovo utente e salvarlo all'interno del database.
+     * Il campo password viene crittografato usando una funzione di hash prima di essere salvato nel database.
+     * @param registerRequest oggetto contenente un campo email e un campo password, che andranno a comporre i rispettivi campi di User.
+     * @throws UserAlreadyPresentException nel caso di utente già registrato.
+     */
     public void createUser(RegisterRequest registerRequest) {
         if(userRepository.existsUserByEmail(registerRequest.getEmail())) {
             throw new UserAlreadyPresentException();
