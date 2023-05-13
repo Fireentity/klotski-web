23af0aca15eb42d41b69c29015bf826774bd77c9
Biblink <alby.bot.24@gmail.com>
2023-05-13 22:45:57 +0200

    Docs: Aggiunta la documentazione per l'interfaccia IRectangularTileStrategy'

--- game-rest-api/src/it/klotski/web/game/services/user/strategy/IRectangularTileStrategy.java
+++ game-rest-api/src/it/klotski/web/game/services/user/strategy/IRectangularTileStrategy.java
@@ -2,6 +2,11 @@ package it.klotski.web.game.services.user.strategy;
 
 import it.klotski.web.game.domain.tile.IRectangularTile;
 
+/**
+ * Interfaccia che utilizza il design pattern "Strategy" che consiste nel creare una famiglia di algoritmi e funzioni,
+ * e incapsularli per renderli intercambiabili, in questo caso i diversi comportamenti associati ai tiles come
+ * l'inserimento e il movimento di essi.
+ */
 public interface IRectangularTileStrategy {
     void apply(IRectangularTile tile);
 }
