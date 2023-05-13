929755f8019dd98c3428f60b374bbf3d7044e0d8
Biblink <alby.bot.24@gmail.com>
2023-05-13 23:31:50 +0200

    Docs: Aggiunta la documentazione per la classe GameRequest

--- game-rest-api/src/it/klotski/web/game/payload/requests/GameRequest.java
+++ game-rest-api/src/it/klotski/web/game/payload/requests/GameRequest.java
@@ -3,6 +3,9 @@ package it.klotski.web.game.payload.requests;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * Classe per la gestione delle richieste di creazione di una nuova partita.
+ */
 @Getter
 @RequiredArgsConstructor
 public class GameRequest {
