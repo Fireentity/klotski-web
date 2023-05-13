e6d4521fa40930ddfbb11878aeb8042a0607d015
Biblink <alby.bot.24@gmail.com>
2023-05-13 17:19:38 +0200

    Docs: Aggiunta la documentazione per la classe WinningTileVisitor'

--- game-rest-api/src/it/klotski/web/game/services/user/visitor/WinningTileVisitor.java
+++ game-rest-api/src/it/klotski/web/game/services/user/visitor/WinningTileVisitor.java
@@ -4,15 +4,25 @@ import it.klotski.web.game.domain.tile.IWinningTile;
 import it.klotski.web.game.services.user.strategy.IRectangularTileStrategy;
 import lombok.Getter;
 
-//TODO complete this class
+/**
+ * Visitor per la classe WinningTile
+ */
 @Getter
 public class WinningTileVisitor implements ITileVisitor {
     private final IRectangularTileStrategy insertionStrategy;
 
+    /**
+     * Costruttore di classe:
+     * @param insertionStrategy strategia generica dei tiles di tipo IWinningTile
+     */
     public WinningTileVisitor(IRectangularTileStrategy insertionStrategy) {
         this.insertionStrategy = insertionStrategy;
     }
 
+    /**
+     * Funzione per la visita degli oggetti di tipo IWinningTile
+     * @param winningTile oggetto che deve essere visitato
+     */
     @Override
     public void visitWinningTile(IWinningTile winningTile) {
         insertionStrategy.apply(winningTile);
