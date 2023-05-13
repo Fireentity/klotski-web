9d33632faef58058c265cc690c1eac3c068313e4
Biblink <alby.bot.24@gmail.com>
2023-05-13 23:53:59 +0200

    Docs: Aggiunta la documentazione per la classe GlobalExceptionHandler

--- game-rest-api/src/it/klotski/web/game/handler/GlobalExceptionHandler.java
+++ game-rest-api/src/it/klotski/web/game/handler/GlobalExceptionHandler.java
@@ -9,24 +9,43 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+/**
+ * Classe per la gestione delle eccezioni.
+ */
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    /**
+     * Funzione per la gestione dell'eccezione UserAlreadyPresentException.
+     * @return alla pagina lo stato CONFLICT e il messaggio di errore "User already registered".
+     */
     @ExceptionHandler(UserAlreadyPresentException.class)
     public ResponseEntity<?> userAlreadyRegisteredException() {
         return ResponseEntity.status(HttpStatus.CONFLICT).body("User already registered");
     }
 
+    /**
+     * Funzione per la gestione dell'eccezione BadCredentialsException.
+     * @return alla pagina lo stato UNAUTHORIZED e il messaggio di errore "Wrong username or password".
+     */
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<?> badCredentialsException() {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED.value()).body("Wrong username or password");
     }
 
+    /**
+     * Funzione per la gestione dell'eccezione ConfigurationNotFoundException.
+     * @return alla pagina lo stato BAD_REQUEST e il messaggio di errore "Unable to find start configuration for that id".
+     */
     @ExceptionHandler(ConfigurationNotFoundException.class)
     public ResponseEntity<?> configurationNotFoundException() {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body("Unable to find start configuration for that id");
     }
 
+    /**
+     * Funzione per la gestione dell'eccezione InvalidBoardConfigurationException.
+     * @return alla pagina lo stato BAD_REQUEST e il messaggio di errore "Invalid board configuration".
+     */
     @ExceptionHandler(InvalidBoardConfigurationException.class)
     public ResponseEntity<?> invalidBoardConfigurationException() {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body("Invalid board configuration");
