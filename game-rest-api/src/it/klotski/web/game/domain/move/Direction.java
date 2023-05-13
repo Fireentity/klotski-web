2c020015858bc6959c2cb8fe3bef87483dea4730
Biblink <alby.bot.24@gmail.com>
2023-05-14 00:05:57 +0200

    Docs: Aggiunta la documentazione per la classe Direction

--- game-rest-api/src/it/klotski/web/game/domain/move/Direction.java
+++ game-rest-api/src/it/klotski/web/game/domain/move/Direction.java
@@ -2,6 +2,9 @@ package it.klotski.web.game.domain.move;
 
 import lombok.Getter;
 
+/**
+ * Enumerazione per le possibili direzioni di movimento dei tiles: destra, sinistra, sopra, sotto.
+ */
 @Getter
 public enum Direction {
     RIGHT(1, 0), LEFT(-1, 0), TOP(0, -1), DOWN(0, 1);
