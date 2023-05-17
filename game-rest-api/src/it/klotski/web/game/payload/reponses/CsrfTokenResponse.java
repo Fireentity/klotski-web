5940b5a2f2ab06f0ae835047a386b99404f6a7eb
Biblink <alby.bot.24@gmail.com>
2023-05-17 10:28:59 +0200

    Docs: Aggiunta la documentazione per la classe CsrfTokenResponse

--- game-rest-api/src/it/klotski/web/game/payload/reponses/CsrfTokenResponse.java
+++ game-rest-api/src/it/klotski/web/game/payload/reponses/CsrfTokenResponse.java
@@ -5,8 +5,26 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
+
+/**
+ * Questa classe rappresenta la risposta del token CSRF.
+ *
+ * La classe CsrfTokenResponse contiene le informazioni relative al token CSRF,
+ * come il nome del parametro, il token stesso e il nome dell'header.
+ */
 public class CsrfTokenResponse {
+    /**
+     * Il nome del parametro del token CSRF.
+     */
     private final String parameterName;
+
+    /**
+     * Il valore del token CSRF.
+     */
     private final String token;
+
+    /**
+     * Il nome dell'header del token CSRF.
+     */
     private final String headerName;
 }
