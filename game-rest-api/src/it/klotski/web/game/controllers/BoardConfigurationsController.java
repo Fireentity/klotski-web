31deb91fd1a2e44b96bfb3d8578870b2b67fa09f
Biblink <alby.bot.24@gmail.com>
2023-05-17 11:09:30 +0200

    Docs: Aggiunta la documentazione per la classe BoardConfigurationsController

--- game-rest-api/src/it/klotski/web/game/controllers/BoardConfigurationsController.java
+++ game-rest-api/src/it/klotski/web/game/controllers/BoardConfigurationsController.java
@@ -18,17 +18,33 @@ import java.util.List;
 import static it.klotski.web.game.constants.ApplicationConstants.ADAPTER_FACTORY;
 import static it.klotski.web.game.constants.ApplicationConstants.START_CONFIGURATIONS_FILE_PATH;
 
+/**
+ * Il controller che gestisce le richieste relative alle configurazioni d'inizio delle board.
+ * Fornisce un endpoint per ottenere la lista delle configurazioni d'inizio disponibili.
+ */
+
 @RestController
 @RequestMapping("/api/configurations")
 public class BoardConfigurationsController {
     private final List<Board> boards;
 
+    /**
+     * Costruisce un'istanza di BoardConfigurationsController.
+     *
+     * @param resourceLoader il ResourceLoader utilizzato per caricare le configurazioni d'inizio
+     * @throws IOException se si verifica un errore durante la lettura del file di configurazione
+     */
     public BoardConfigurationsController(ResourceLoader resourceLoader) throws IOException {
         Gson gson = new GsonBuilder().registerTypeAdapterFactory(ADAPTER_FACTORY).create();
         File file = resourceLoader.getResource(START_CONFIGURATIONS_FILE_PATH).getFile();
         this.boards = gson.fromJson(Files.readString(file.toPath()), new TypeToken<List<Board>>(){}.getType());
     }
 
+    /**
+     * Restituisce una ResponseEntity contenente la lista delle configurazioni d'inizio delle board.
+     *
+     * @return una ResponseEntity contenente la lista delle configurazioni d'inizio delle board
+     */
     @GetMapping
     public ResponseEntity<List<Board>> getStartConfigurations() {
         return ResponseEntity.ok(boards);
