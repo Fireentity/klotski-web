72b622a03ad675477a46141e8edc3ef5ece83092
Biblink <alby.bot.24@gmail.com>
2023-05-17 11:51:45 +0200

    Docs: Aggiunta la documentazione per la classe Board

--- game-rest-api/src/it/klotski/web/game/configs/Board.java
+++ game-rest-api/src/it/klotski/web/game/configs/Board.java
@@ -7,12 +7,31 @@ import lombok.With;
 
 import java.util.TreeSet;
 
+/**
+ * Rappresenta la griglia di gioco.
+ */
 @With
 @Getter
 @RequiredArgsConstructor
 public class Board {
+
+    /**
+     * Larghezza del tabellone.
+     */
     private final int boardWidth;
+
+    /**
+     * Altezza del tabellone.
+     */
     private final int boardHeight;
+
+    /**
+     * ID del tabellone.
+     */
     private final int id;
+
+    /**
+     * Insieme ordinato di tessere (tiles) presenti nel tabellone.
+     */
     private final TreeSet<ITile> tiles;
 }
