148ecf88d812776d3566248c6a5c514379c5bd84
Biblink <alby.bot.24@gmail.com>
2023-05-17 11:09:19 +0200

    Docs: Aggiunta la documentazione per la classe CsrfController

--- game-rest-api/src/it/klotski/web/game/controllers/CsrfController.java
+++ game-rest-api/src/it/klotski/web/game/controllers/CsrfController.java
@@ -7,10 +7,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Il controller che gestisce le richieste relative al token CSRF.
+ * Fornisce un endpoint per ottenere il token CSRF corrente.
+ */
+
 @RestController
 @RequestMapping("/api/csrf")
 public class CsrfController {
 
+    /**
+     * Restituisce un oggetto CsrfTokenResponse contenente i dettagli del token CSRF.
+     *
+     * @param request l'oggetto HttpServletRequest
+     * @return un oggetto CsrfTokenResponse contenente il nome del parametro CSRF, il token CSRF e il nome dell'header CSRF
+     */
     @GetMapping
     public CsrfTokenResponse getCsrfToken(HttpServletRequest request) {
         CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
