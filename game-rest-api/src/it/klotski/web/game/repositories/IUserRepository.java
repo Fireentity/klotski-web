21b6a301451afffe75da5ef3d492b93958a411cb
Biblink <alby.bot.24@gmail.com>
2023-05-17 10:46:02 +0200

    Docs: Aggiunta la documentazione per l'interfaccia IUserRepository

--- game-rest-api/src/it/klotski/web/game/repositories/IUserRepository.java
+++ game-rest-api/src/it/klotski/web/game/repositories/IUserRepository.java
@@ -6,8 +6,31 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/**
+ * Repository per l'accesso ai dati degli utenti.
+ *
+ * L'interfaccia `IUserRepository` estende l'interfaccia `JpaRepository` di Spring Data JPA per fornire
+ * operazioni di accesso ai dati degli utenti.
+ *
+ * Viene utilizzato l'annotazione `@Repository` per indicare che questa interfaccia è un componente di
+ * repository gestito da Spring.
+ */
 @Repository
 public interface IUserRepository extends JpaRepository<User, Long> {
+
+    /**
+     * Trova un utente tramite l'indirizzo email.
+     *
+     * @param email l'indirizzo email dell'utente da cercare
+     * @return un'istanza di `Optional<User>` contenente l'utente trovato (se presente), altrimenti un'istanza vuota
+     */
     Optional<User> findByEmail(String email);
+
+    /**
+     * Verifica l'esistenza di un utente tramite l'indirizzo email.
+     *
+     * @param email l'indirizzo email dell'utente da verificare
+     * @return `true` se l'utente esiste, `false` altrimenti
+     */
     boolean existsUserByEmail(String email);
 }
