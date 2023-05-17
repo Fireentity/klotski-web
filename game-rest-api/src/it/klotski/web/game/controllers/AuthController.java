19bf45c2ba63dcaad9281be20dc5f2db18e5c8dd
Biblink <alby.bot.24@gmail.com>
2023-05-17 11:02:45 +0200

    Docs: Aggiunta la documentazione per la classe AuthController

--- game-rest-api/src/it/klotski/web/game/controllers/AuthController.java
+++ game-rest-api/src/it/klotski/web/game/controllers/AuthController.java
@@ -21,14 +21,40 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * La classe AuthController gestisce le operazioni di autenticazione e registrazione degli utenti.
+ * Fornisce endpoint per il login e la registrazione.
+ */
+
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+    /**
+     * Il servizio utente utilizzato per gestire le operazioni legate agli utenti.
+     */
     private final UserService userService;
+
+    /**
+     * Il gestore di autenticazione utilizzato per autenticare gli utenti.
+     */
     private final AuthenticationManager authenticationManager;
+
+    /**
+     * Il repository del contesto di sicurezza utilizzato per gestire il contesto di sicurezza delle richieste.
+     */
     private final SecurityContextRepository securityContextRepository = new HttpSessionSecurityContextRepository();
+
+    /**
+     * La strategia di gestione del contesto di sicurezza utilizzata per recuperare e impostare il contesto di sicurezza.
+     */
     private final SecurityContextHolderStrategy securityContextHolderStrategy = SecurityContextHolder.getContextHolderStrategy();
 
+    /**
+     * Costruisce un'istanza di AuthController con i servizi utente e il gestore di autenticazione specificati.
+     *
+     * @param userService             il servizio utente utilizzato per gestire le operazioni legate agli utenti
+     * @param authenticationManager   il gestore di autenticazione utilizzato per autenticare gli utenti
+     */
     @Autowired
     public AuthController(UserService userService,
                           AuthenticationManager authenticationManager) {
@@ -36,6 +62,14 @@ public class AuthController {
         this.authenticationManager = authenticationManager;
     }
 
+    /**
+     * Metodo per autenticare un utente.
+     *
+     * @param loginRequest la richiesta di login contenente email e password dell'utente
+     * @param request      l'oggetto HttpServletRequest
+     * @param response     l'oggetto HttpServletResponse
+     * @return una ResponseEntity contenente un messaggio di successo in caso di autenticazione avvenuta con successo
+     */
     @PostMapping("/login")
     public ResponseEntity<String> authenticateUser(@RequestBody LoginRequest loginRequest,
                                                    HttpServletRequest request,
@@ -47,12 +81,18 @@ public class AuthController {
         context.setAuthentication(authentication);
         securityContextHolderStrategy.setContext(context);
         securityContextRepository.saveContext(context, request, response);
-        return new ResponseEntity<>("User signed-in successfully!.", HttpStatus.OK);
+        return new ResponseEntity<>("L'utente si è autenticato con successo.", HttpStatus.OK);
     }
 
+    /**
+     * Metodo per registrare un nuovo utente.
+     *
+     * @param registerRequest la richiesta di registrazione contenente i dati dell'utente
+     * @return una ResponseEntity contenente un messaggio di successo in caso di registrazione avvenuta con successo
+     */
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody RegisterRequest registerRequest) {
         userService.createUser(registerRequest);
-        return ResponseEntity.ok("User registered successfully");
+        return ResponseEntity.ok("Utente registrato con successo");
     }
 }
