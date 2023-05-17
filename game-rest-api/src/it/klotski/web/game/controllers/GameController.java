7e375ed8af3e68ecf0a8b19d529fb38a0cf63273
Biblink <alby.bot.24@gmail.com>
2023-05-17 11:40:51 +0200

    Docs: Aggiunta la documentazione per la classe GameController

--- game-rest-api/src/it/klotski/web/game/controllers/GameController.java
+++ game-rest-api/src/it/klotski/web/game/controllers/GameController.java
@@ -15,28 +15,52 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+/**
+ * Il controller che gestisce le richieste relative ai giochi.
+ * Fornisce endpoint per creare un nuovo gioco, ottenere i dettagli di un gioco specifico
+ * e recuperare l'ultimo gioco non completato dell'utente autenticato.
+ */
+
 @RestController
 @RequestMapping(path = "/api/games")
 public class GameController {
     private final IPuzzleService puzzleService;
 
+    /**
+     * Costruisce un'istanza di GameController con il servizio di puzzle specificato.
+     *
+     * @param puzzleService il servizio di puzzle utilizzato per gestire le operazioni di gioco
+     */
     @Autowired
     public GameController(IPuzzleService puzzleService) {
         this.puzzleService = puzzleService;
     }
 
+    /**
+     * Crea un nuovo gioco a partire da una configurazione d'inizio specificata.
+     *
+     * @param gameRequest la richiesta di gioco contenente l'identificatore della configurazione d'inizio
+     * @return una ResponseEntity contenente il GameResponse con i dettagli del gioco creato
+     */
     @PostMapping
     public ResponseEntity<GameResponse> createGame(@RequestBody GameRequest gameRequest) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return ResponseEntity.ok(puzzleService.createGameFromConfiguration(authentication.getName(), gameRequest.getStartConfigId()));
     }
 
+    /**
+     * Restituisce i dettagli di un gioco specificato dall'identificatore del gioco.
+     *
+     * @param gameId l'identificatore del gioco
+     * @return una ResponseEntity contenente il GameResponse con i dettagli del gioco richiesto
+     * @throws GameNotFoundException se il gioco non viene trovato
+     */
     @GetMapping
     public ResponseEntity<GameResponse> getGame(@RequestParam long gameId) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         User user = (User) authentication.getPrincipal();
         Game game = puzzleService.findGameById(gameId).orElseThrow(GameNotFoundException::new);
-        if(game.getPlayer().getId() != user.getId()) {
+        if (game.getPlayer().getId() != user.getId()) {
             throw new GameNotFoundException();
         }
 
@@ -44,6 +68,11 @@ public class GameController {
         return ResponseEntity.ok(GameResponse.from(game, board));
     }
 
+    /**
+     * Restituisce i dettagli dell'ultimo gioco non completato dell'utente autenticato.
+     *
+     * @return una ResponseEntity contenente il GameResponse con i dettagli dell'ultimo gioco non completato
+     */
     @GetMapping(path = "/unfinished")
     public ResponseEntity<GameResponse> getLastUnfinishedGame() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
