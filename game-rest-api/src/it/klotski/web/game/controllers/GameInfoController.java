9dcf84449b4267809e1c5481c88b61ffa0709b63
Biblink <alby.bot.24@gmail.com>
2023-05-17 11:40:40 +0200

    Docs: Aggiunta la documentazione per la classe GameInfoController

--- game-rest-api/src/it/klotski/web/game/controllers/GameInfoController.java
+++ game-rest-api/src/it/klotski/web/game/controllers/GameInfoController.java
@@ -14,15 +14,32 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Il controller che gestisce le richieste relative alle informazioni dei giochi dell'utente.
+ * Fornisce un endpoint per ottenere le informazioni sui giochi dell'utente autenticato.
+ */
+
 @RestController
 @RequestMapping(path = "/api/game/info")
 public class GameInfoController {
     private final IPuzzleService puzzleService;
 
+    /**
+     * Costruisce un'istanza di GameInfoController con il servizio di puzzle specificato.
+     *
+     * @param puzzleService il servizio di puzzle utilizzato per gestire le operazioni di gioco
+     */
     public GameInfoController(IPuzzleService puzzleService) {
         this.puzzleService = puzzleService;
     }
 
+    /**
+     * Restituisce una lista di GameInfoResponse contenente le informazioni sui giochi dell'utente autenticato.
+     *
+     * @param page il numero di pagina (predefinito: 0)
+     * @param size la dimensione della pagina (predefinito: 10)
+     * @return una ResponseEntity contenente la lista di GameInfoResponse con le informazioni sui giochi
+     */
     @GetMapping
     public ResponseEntity<List<GameInfoResponse>> getGames(@RequestParam(defaultValue = "0") int page,
                                                            @RequestParam(defaultValue = "10") int size) {
