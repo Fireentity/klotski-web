c1c2f751d95e249a1b55d4cb9f5dad635375175d
Biblink <alby.bot.24@gmail.com>
2023-05-17 11:51:29 +0200

    Docs: Aggiunta la documentazione per la classe ApplicationConstants

--- game-rest-api/src/it/klotski/web/game/constants/ApplicationConstants.java
+++ game-rest-api/src/it/klotski/web/game/constants/ApplicationConstants.java
@@ -7,10 +7,29 @@ import it.klotski.web.game.utils.RuntimeTypeAdapterFactory;
 
 import java.text.SimpleDateFormat;
 
+/**
+ * Questa classe contiene le costanti utilizzate nell'applicazione.
+ */
 public class ApplicationConstants {
+
+    /**
+     * La dimensione predefinita della pagina utilizzata per la paginazione.
+     */
     public static final int DEFAULT_PAGE_SIZE = 20;
+
+    /**
+     * Il formato della data utilizzato nell'applicazione.
+     */
     public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy");
+
+    /**
+     * Il percorso del file di configurazione delle configurazioni di partenza.
+     */
     public static final String START_CONFIGURATIONS_FILE_PATH = "classpath:config/startConfigurations.json";
+
+    /**
+     * Factory di runtime per il tipo di adattatore per le istanze di ITile.
+     */
     public static final RuntimeTypeAdapterFactory<ITile> ADAPTER_FACTORY = RuntimeTypeAdapterFactory.of(ITile.class, "type")
             .registerSubtype(RectangularTile.class, "rectangular")
             .registerSubtype(WinningTile.class, "winning");
