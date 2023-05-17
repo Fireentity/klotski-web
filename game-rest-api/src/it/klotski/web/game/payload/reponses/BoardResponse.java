cdecafe9e46cc72a51d55b9dc8ce5987d03a2459
Biblink <alby.bot.24@gmail.com>
2023-05-17 10:28:21 +0200

    Docs: Aggiunta la documentazione per la classe BoardResponse

--- game-rest-api/src/it/klotski/web/game/payload/reponses/BoardResponse.java
+++ game-rest-api/src/it/klotski/web/game/payload/reponses/BoardResponse.java
@@ -8,6 +8,16 @@ import java.util.List;
 
 @Getter
 @RequiredArgsConstructor
+
+/**
+ * Questa classe rappresenta la risposta del tabellone di gioco.
+ *
+ * La classe BoardResponse contiene una lista di oggetti ITile che rappresentano
+ * i tessere del tabellone di gioco.
+ */
 public class BoardResponse {
+    /**
+     * La lista delle tessere del tabellone di gioco.
+     */
     private final List<ITile> tiles;
 }
