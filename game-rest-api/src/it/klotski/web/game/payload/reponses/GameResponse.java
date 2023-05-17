9e97e9ff9877d2012a27521a03dd8d480286bc90
Biblink <alby.bot.24@gmail.com>
2023-05-17 10:29:41 +0200

    Docs: Aggiunta la documentazione per la classe GameResponse

--- game-rest-api/src/it/klotski/web/game/payload/reponses/GameResponse.java
+++ game-rest-api/src/it/klotski/web/game/payload/reponses/GameResponse.java
@@ -9,14 +9,52 @@ import static it.klotski.web.game.constants.ApplicationConstants.DATE_FORMAT;
 
 @Getter
 @RequiredArgsConstructor
+
+/**
+ * Questa classe rappresenta la risposta di un gioco completo.
+ *
+ * La classe GameResponse contiene le informazioni complete di un gioco, inclusi l'identificatore,
+ * l'ID della configurazione di partenza, la data di creazione formattata, la durata del gioco,
+ * lo stato di completamento e il tabellone di gioco.
+ */
 public class GameResponse {
+    /**
+     * L'identificatore del gioco.
+     */
     private final long id;
+
+    /**
+     * L'ID della configurazione di partenza del gioco.
+     */
     private final int startConfigurationId;
+
+    /**
+     * La data di creazione del gioco formattata come stringa.
+     */
     private final String date;
+
+    /**
+     * La durata del gioco in millisecondi.
+     */
     private final long duration;
+
+    /**
+     * Lo stato di completamento del gioco.
+     */
     private final boolean finished;
+
+    /**
+     * Il tabellone di gioco.
+     */
     private final Board board;
 
+    /**
+     * Crea un'istanza di GameResponse a partire da un oggetto Game e un tabellone di gioco.
+     *
+     * @param game  L'oggetto Game da cui ottenere le informazioni.
+     * @param tiles Il tabellone di gioco.
+     * @return Un'istanza di GameResponse contenente le informazioni complete del gioco.
+     */
     public static GameResponse from(Game game, Board tiles) {
         return new GameResponse(game.getId(),
                 game.getStartConfigurationId(),
@@ -26,6 +64,12 @@ public class GameResponse {
                 tiles);
     }
 
+    /**
+     * Verifica se l'oggetto GameResponse è uguale a un altro oggetto.
+     *
+     * @param o L'oggetto da confrontare con l'istanza corrente di GameResponse.
+     * @return true se gli oggetti sono uguali, false altrimenti.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -41,6 +85,11 @@ public class GameResponse {
         return getBoard().equals(that.getBoard());
     }
 
+    /**
+     * Restituisce l'hashCode dell'oggetto GameResponse.
+     *
+     * @return L'hashCode calcolato per l'istanza corrente di GameResponse.
+     */
     @Override
     public int hashCode() {
         int result = (int) (getId() ^ (getId() >>> 32));
