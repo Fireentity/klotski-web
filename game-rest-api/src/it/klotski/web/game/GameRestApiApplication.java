2578b1f84766d1327a3b7398c9123453fc7b761c
Biblink <alby.bot.24@gmail.com>
2023-05-17 10:38:29 +0200

    Docs: Aggiunta la documentazione per la classe GameRestApiApplication

--- game-rest-api/src/it/klotski/web/game/GameRestApiApplication.java
+++ game-rest-api/src/it/klotski/web/game/GameRestApiApplication.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
 @SpringBootApplication(scanBasePackages = {"it.klotski.web.game.*"})
+
+/**
+ * Classe principale dell'applicazione REST API del gioco Klotski.
+ *
+ * La classe GameRestApiApplication è la classe principale dell'applicazione REST API del gioco Klotski.
+ * Avvia l'applicazione Spring Boot e gestisce l'avvio del server.
+ */
 public class GameRestApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(GameRestApiApplication.class, args);
