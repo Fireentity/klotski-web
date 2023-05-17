7b780d8eeebe1186541a935ef9f91c7a8e565f3a
Biblink <alby.bot.24@gmail.com>
2023-05-17 10:45:54 +0200

    Docs: Aggiunta la documentazione per l'interfaccia IRectangularTileStrategy

--- game-rest-api/src/it/klotski/web/game/services/user/strategy/IRectangularTileStrategy.java
+++ game-rest-api/src/it/klotski/web/game/services/user/strategy/IRectangularTileStrategy.java
@@ -3,10 +3,17 @@ package it.klotski.web.game.services.user.strategy;
 import it.klotski.web.game.domain.tile.IRectangularTile;
 
 /**
- * Interfaccia che utilizza il design pattern "Strategy" che consiste nel creare una famiglia di algoritmi e funzioni,
- * e incapsularli per renderli intercambiabili, in questo caso i diversi comportamenti associati ai tiles come
- * l'inserimento e il movimento di essi.
+ * Interfaccia che utilizza il design pattern "Strategy" per definire il comportamento associato ai tile rettangolari.
+ *
+ * L'interfaccia IRectangularTileStrategy definisce il metodo `apply`, che rappresenta l'algoritmo o la funzione
+ * specifica da applicare a un tile rettangolare. L'obiettivo è rendere il comportamento dei tile rettangolari
+ * intercambiabile, consentendo di utilizzare diverse strategie per l'inserimento e il movimento dei tile.
  */
 public interface IRectangularTileStrategy {
+    /**
+     * Applica la strategia al tile rettangolare specificato.
+     *
+     * @param tile il tile rettangolare a cui applicare la strategia
+     */
     void apply(IRectangularTile tile);
 }
