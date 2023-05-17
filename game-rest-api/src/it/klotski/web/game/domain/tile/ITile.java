fb3bab1bafeff9e9ee6609f2f8710724db3316fd
Biblink <alby.bot.24@gmail.com>
2023-05-17 10:34:20 +0200

    Docs: Aggiunta la documentazione per l'interfaccia ITile

--- game-rest-api/src/it/klotski/web/game/domain/tile/ITile.java
+++ game-rest-api/src/it/klotski/web/game/domain/tile/ITile.java
@@ -3,11 +3,30 @@ package it.klotski.web.game.domain.tile;
 import it.klotski.web.game.domain.move.Direction;
 import it.klotski.web.game.services.user.visitor.ITileVisitor;
 
+/**
+ * Questa interfaccia rappresenta una tessera del gioco.
+ */
 public interface ITile extends Comparable<ITile> {
+    /**
+     * Restituisce l'ID della tessera.
+     *
+     * @return l'ID della tessera
+     */
     int getId();
 
+    /**
+     * Accetta un visitor per la tessera.
+     *
+     * @param visitor il visitor
+     */
     void accept(ITileVisitor visitor);
 
+    /**
+     * Sposta la tessera in una direzione specificata.
+     *
+     * @param direction la direzione in cui spostare la tessera
+     * @return la tessera spostata
+     */
     RectangularTile move(Direction direction);
 
 }
