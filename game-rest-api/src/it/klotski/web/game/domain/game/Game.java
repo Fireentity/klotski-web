a0d5180e3d7208e17f5263990fc5c81fb9f75397
Biblink <alby.bot.24@gmail.com>
2023-05-17 10:29:20 +0200

    Docs: Aggiunta la documentazione per la classe Game

--- game-rest-api/src/it/klotski/web/game/domain/game/Game.java
+++ game-rest-api/src/it/klotski/web/game/domain/game/Game.java
@@ -9,8 +9,11 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.sql.Date;
 
 /**
- * Classe che rappresenta l'oggetto "partita", costituito da: id, user, configurazione iniziale, data di creazione, data
- * di aggiornamento, numero di mosse, stato (finita o in corso).
+ * Questa classe rappresenta un gioco Klotski.
+ *
+ * Un oggetto Game contiene informazioni sul gioco, come l'identificatore, il giocatore associato,
+ * l'ID della configurazione di partenza, la durata del gioco, le informazioni sulla creazione e l'aggiornamento,
+ * il numero di mosse effettuate e lo stato di completamento del gioco.
  */
 @Setter
 @Entity
@@ -18,17 +21,48 @@ import java.sql.Date;
 @Table(name = "games")
 @NoArgsConstructor
 public class Game {
+    /**
+     * Identificatore univoco del gioco.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    /**
+     * Il giocatore associato al gioco.
+     */
     @OneToOne
     private User player;
+
+    /**
+     * L'ID della configurazione di partenza del gioco.
+     */
     private int startConfigurationId;
+
+    /**
+     * La durata del gioco in millisecondi.
+     */
     private long duration;
+
+    /**
+     * Data e ora di creazione del gioco.
+     */
     @CreationTimestamp
     private Date createdAt;
+
+    /**
+     * Data e ora di aggiornamento del gioco.
+     */
     @UpdateTimestamp
     private Date updatedAt;
+
+    /**
+     * Il numero di mosse effettuate nel gioco.
+     */
     private int moves;
+
+    /**
+     * Lo stato di completamento del gioco.
+     */
     private boolean finished;
 }
