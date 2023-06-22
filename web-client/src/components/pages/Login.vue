dc8ae3ff7eb048dd60ff93093ab39ee0912e64c8
Fireentity <croceclaudio57@gmail.com>
2023-06-22 17:20:36 +0200

    Chore: Rimossi file intili e cancellata la variabile dal file .env per il docker di game-rest-api

--- web-client/src/components/pages/Login.vue
+++ web-client/src/components/pages/Login.vue
@@ -46,8 +46,7 @@ const submit = () => {
 <template>
     <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your
-                account</h2>
+            <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Accedi al tuo account</h2>
         </div>
 
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
